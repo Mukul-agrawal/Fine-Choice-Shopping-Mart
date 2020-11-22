e0fc70008eff84fea616645fb65b441509de98ca
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-11-22 22:08:40 +0530

    add item.php

---
+++ public/login_img/item.php
@@ -0,0 +1,233 @@
+<?php require_once("../resources/config.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Hometown Grocers - Description</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel='stylesheet' type="text/css" href='proj.css'/>
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel='stylesheet' type="text/css" href="customelogin.css"> -->
+
+</head>
+<?php include(TEMPLATE_FRONT . DS . "headers.php") ?>
+
+<!--Row For Image and Short Description-->
+
+<?php
+$query = query(" SELECT * FROM products where product_id = " . escape_string($_GET['id']) . " ");
+    confirm($query);
+
+while($row = fetch_array($query)):
+
+   
+
+
+?>
+
+
+ <div class="col-md-9">
+<div class="row">
+
+    <div class="col-md-7">
+       <img  width="300" class="img-responsive" src="../resources/<?php echo display_image($row['product_image']); ?>" alt="">
+
+    </div>
+
+    <div class="col-md-5">
+
+        <div class="thumbnail">
+         
+
+    <div class="caption-full">
+        <h4><a href="#"><?php echo $row['product_title']; ?></a> </h4>
+        <hr>
+        <h4 class=""><?php echo "Rs" . $row['product_price']; ?></h4>
+
+    <div class="ratings">
+     
+        <p>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star-empty"></span>
+            4.0 stars
+        </p>
+    </div>
+          
+        <p><?php echo $row['short_description']; ?></p>
+
+   
+    <form action="">
+        <div class="form-group">
+           <a href="../resources/cart.php?add=<?php echo $row['product_id'];?>" class="btn btn-primary">ADD</a>
+        </div>
+    </form>
+
+    </div>
+ 
+</div>
+
+</div>
+
+
+</div><!--Row For Image and Short Description-->
+
+
+        <hr>
+
+
+<!--Row for Tab Panel-->
+
+<div class="row">
+
+<div role="tabpanel">
+
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Description</a></li>
+    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Reviews</a></li>
+
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="home">
+
+<p></p>
+           
+        <p><?php echo $row['product_description']; ?></p>
+
+    </div>
+    <div role="tabpanel" class="tab-pane" id="profile">
+
+  <div class="col-md-6">
+
+       <h3>2 Reviews From </h3>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-md-12">
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star-empty"></span>
+                Anonymous
+                <span class="pull-right">10 days ago</span>
+                <p>This product was great in terms of quality. I would definitely buy another!</p>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-md-12">
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star-empty"></span>
+                Anonymous
+                <span class="pull-right">12 days ago</span>
+                <p>I've alredy ordered another one!</p>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-md-12">
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-star-empty"></span>
+                Anonymous
+                <span class="pull-right">15 days ago</span>
+                <p>I've seen some better than this, but not at this price. I definitely recommend this item.</p>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="col-md-6">
+        <h3>Add A review</h3>
+
+     <form action="" class="form-inline">
+        <div class="form-group">
+            <label for="">Name</label>
+                <input type="text" class="form-control" >
+            </div>
+             <div class="form-group">
+            <label for="">Email</label>
+                <input type="test" class="form-control">
+            </div>
+
+        <div>
+            <h3>Your Rating</h3>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star"></span>
+        </div>
+
+            <br>
+            
+             <div class="form-group">
+             <textarea name="" id="" cols="60" rows="10" class="form-control"></textarea>
+            </div>
+
+             <br>
+              <br>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="SUBMIT">
+            </div>
+        </form>
+
+    </div>
+
+ </div>
+
+ </div>
+
+</div>
+
+
+</div><!--Row for Tab Panel-->
+
+
+
+
+</div>
+<?php endwhile; ?>
+
+</div>
+</html>
+    <!-- /.container -->
+
+   
